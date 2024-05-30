3fd36e66783e4a96f6fff316cbc26e26ec08b00f
Jamie Hardt <jamiehardt@me.com>
2024-05-29 20:41:07 -0700

    Playing with the tests.

--- src/adm.zig
+++ src/adm.zig
@@ -4,10 +4,16 @@ const xml = @cImport({
     @cInclude("libxml2/libxml/tree.h");
 });
 
-test "open xml document" {
+test "parse XML from data buffer" {
     const buffer: [*:0]const u8 = "<x>a</x>";
     const doc: xml.xmlDocPtr = xml.xmlReadMemory(buffer, 8, null, "utf-8", 0);
+    defer xml.xmlCleanupParser();
+    defer xml.xmlFreeDoc(doc);
+
     const root: ?xml.xmlNodePtr = xml.xmlDocGetRootElement(doc);
-    std.debug.print("{any}", .{root});
+
     try std.testing.expect(root != null);
+    if (root) |r| {
+        try std.testing.expect(std.mem.eql(u8, std.mem.span(r.*.name), "x"));
+    }
 }
