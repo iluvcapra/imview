54dced98bc80488c9a41807900d9f5ee475828aa
Jamie Hardt <jamiehardt@me.com>
2024-05-31 00:05:33 -0700

    Implementing XML parsing

--- src/adm.zig
+++ src/adm.zig
@@ -1,9 +1,55 @@
 const std = @import("std");
+const AnyWriter = std.io.AnyWriter;
 const wave = @import("wave.zig");
 const xml = @cImport({
     @cInclude("libxml2/libxml/parser.h");
     @cInclude("libxml2/libxml/tree.h");
+    @cInclude("libxml2/libxml/xpath.h");
+    @cInclude("libxml2/libxml/xpathInternals.h");
+    @cInclude("libxml2/libxml/xmlmemory.h");
 });
+
+const ADMErrors = error{ XmlParseError, AdmXmlEntityError };
+
+fn print_audio_programme_data(doc: xml.xmlDocPtr, xpath_ctx: xml.xmlXPathContextPtr, writer: AnyWriter) !void {
+    if (xml.xmlXPathEvalExpression("//adm:audioProgramme[1]", xpath_ctx)) |result| {
+        defer xml.xmlXPathFreeObject(result);
+        if (result.*.nodesetval.*.nodeNr != 1) {
+            return error.AdmXmlEntityError;
+        } else {
+            const audio_programme_node = result.*.nodesetval.*.nodeTab[0];
+            try writer.print("Audio Programme:\n", .{});
+
+            var this_attr = audio_programme_node.*.properties;
+            while (this_attr != null) : (this_attr = this_attr.*.next) {
+                const value = xml.xmlNodeListGetString(doc, this_attr.*.children, 1);
+                defer xml.xmlFree.?(value);
+                try writer.print(" -- {s} = {s}\n", .{ this_attr.*.name, value });
+            }
+        }
+    }
+}
+
+pub fn print_adm_xml_summary(adm_xml: []const u8, writer: AnyWriter) !void {
+    xml.xmlInitParser();
+    defer xml.xmlCleanupParser();
+
+    const doc: xml.xmlDocPtr = xml.xmlReadMemory(@ptrCast(adm_xml), @intCast(adm_xml.len), null, "utf-8", 0) orelse {
+        return error.XmlParseError;
+    };
+    defer xml.xmlFreeDoc(doc);
+
+    const xpath_ctx: xml.xmlXPathContextPtr = xml.xmlXPathNewContext(doc) orelse {
+        @panic("xmlXPathNewContext Failed!");
+    };
+    defer xml.xmlXPathFreeContext(xpath_ctx);
+
+    if (xml.xmlXPathRegisterNs(xpath_ctx, "adm", "urn:ebu:metadata-schema:ebuCore_2016") != 0) {
+        @panic("xmlXPathRegisterNs Failed!");
+    }
+
+    try print_audio_programme_data(doc, xpath_ctx, writer);
+}
 
 test "parse XML from data buffer" {
     const buffer: [*:0]const u8 = "<x>a</x>";
@@ -18,3 +64,10 @@ test "parse XML from data buffer" {
         try std.testing.expect(std.mem.eql(u8, std.mem.span(r.*.name), "x"));
     }
 }
+
+// test "XML error from data buffer" {
+//     const buffer: [*:0]const u8 = "<xa</x>";
+//     const doc: ?xml.xmlDocPtr = xml.xmlReadMemory(buffer, 8, null, "utf-8", 0);
+//     defer xml.xmlCleanupParser();
+//     defer xml.xmlFreeDoc(doc);
+// }
--- src/main.zig
+++ src/main.zig
@@ -3,27 +3,15 @@ pub const adm = @import("adm.zig");
 pub const wave = @import("wave.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+    if (try wave.read_chunk("test_audio/adm.wav", "axml", allocator)) |adm_xml| {
+        defer allocator.free(adm_xml);
+        try adm.print_adm_xml_summary(adm_xml, std.io.getStdOut().writer().any());
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
 test {
     std.testing.refAllDecls(@This());
 }
