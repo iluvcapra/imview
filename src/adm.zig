5d2a84750bbe511c68eca44c1aeefe831967b9cb
Jamie Hardt <jamiehardt@me.com>
2024-06-01 13:16:06 -0700

    Refactoring and implementing some xml helpers

--- src/adm.zig
+++ src/adm.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 const AnyWriter = std.io.AnyWriter;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+const xpath_string_value = @import("xml_additions.zig").xpath_string_value;
 const wave = @import("wave.zig");
 const xml = @cImport({
     @cInclude("libxml2/libxml/parser.h");
@@ -9,30 +12,86 @@ const xml = @cImport({
     @cInclude("libxml2/libxml/xmlmemory.h");
 });
 
-const ADMErrors = error{ XmlParseError, AdmXmlEntityError };
+const AdmAudioProgramme = struct {
+    audioProgrammeID: []const u8,
+    audioProgrammeName: []const u8,
+    start: []const u8,
+    end: []const u8,
+    allocator: Allocator,
 
-fn print_audio_programme_data(doc: xml.xmlDocPtr, xpath_ctx: xml.xmlXPathContextPtr, writer: AnyWriter) !void {
-    if (xml.xmlXPathEvalExpression("//adm:audioProgramme[1]", xpath_ctx)) |result| {
-        defer xml.xmlXPathFreeObject(result);
-        if (result.*.nodesetval.*.nodeNr != 1) {
-            return error.AdmXmlEntityError;
-        } else {
-            const audio_programme_node = result.*.nodesetval.*.nodeTab[0];
-            try writer.print("Audio Programme:\n", .{});
+    fn init(allocator: Allocator, xpath_ctx: xml.xmlXPathContextPtr) @This() {
+        const audioProgrammeID = xpath_string_value("string(//adm:audioFormatExtended/adm:audioProgramme[1]/@audioProgrammeID)", xpath_ctx, allocator);
+        const audioProgrammeName = xpath_string_value("string(//*/adm:audioProgramme[1]/@audioProgrammeName)", xpath_ctx, allocator);
+        const start = xpath_string_value("string(//*/adm:audioProgramme[1]/@start)", xpath_ctx, allocator);
+        const end = xpath_string_value("string(//*/adm:audioProgramme[1]/@end)", xpath_ctx, allocator);
 
-            var this_attr = audio_programme_node.*.properties;
-            while (this_attr != null) : (this_attr = this_attr.*.next) {
-                const value = xml.xmlNodeListGetString(doc, this_attr.*.children, 1);
-                defer xml.xmlFree.?(value);
-                try writer.print(" -- {s} = {s}\n", .{ this_attr.*.name, value });
-            }
-        }
+        return @This(){
+            .audioProgrammeID = audioProgrammeID,
+            .audioProgrammeName = audioProgrammeName,
+            .start = start,
+            .end = end,
+            .allocator = allocator,
+        };
     }
-}
+
+    fn print(self: @This(), writer: AnyWriter) !void {
+        try writer.print("Audio Programme ID : {s}\n", .{self.audioProgrammeID});
+        try writer.print(" - Programme Name  : {s}\n", .{self.audioProgrammeName});
+        try writer.print(" - Start           : {s}\n", .{self.start});
+        try writer.print(" - End             : {s}\n", .{self.end});
+    }
+
+    fn deinit(self: @This()) void {
+        self.allocator.free(self.audioProgrammeID);
+        self.allocator.free(self.audioProgrammeName);
+        self.allocator.free(self.start);
+        self.allocator.free(self.end);
+    }
+};
+
+// const AdmAudioContent = struct {
+//     audioContentID: []const u8,
+//     audioContentName: []const u8,
+//     audioObjectIDs: ArrayList([]const u8),
+//     allocator: Allocator,
+//
+//     fn init(allocator: Allocator, xpath_ctx: xml.xmlXPathContextPtr) !@This() {
+//         const audioProgrammeID = try xpath_string_value("string(//adm:audioFormatExtended/adm:audioProgramme[1]/@audioProgrammeID)", xpath_ctx, allocator);
+//         const audioProgrammeName = try xpath_string_value("string(//*/adm:audioProgramme[1]/@audioProgrammeName)", xpath_ctx, allocator);
+//         const start = try xpath_string_value("string(//*/adm:audioProgramme[1]/@start)", xpath_ctx, allocator);
+//         const end = try xpath_string_value("string(//*/adm:audioProgramme[1]/@end)", xpath_ctx, allocator);
+//
+//         return @This(){
+//             .audioProgrammeID = audioProgrammeID,
+//             .audioProgrammeName = audioProgrammeName,
+//             .start = start,
+//             .end = end,
+//             .allocator = allocator,
+//         };
+//     }
+//
+//     fn print(self: @This(), writer: AnyWriter) !void {
+//         try writer.print("Audio Content ID   : {s}\n", .{self.audioContentID});
+//         try writer.print(" - Content Name    : {s}\n", .{self.audioContentName});
+//     }
+//
+//     fn deinit(self: @This()) void {
+//         self.allocator.free(self.audioContentID);
+//         self.allocator.free(self.audioContentName);
+//         for (audioObjectIDs) |obj| {
+//             self.allocator.free(obj);
+//         }
+//         self.audioObjectIDs.deinit();
+//     }
+// };
 
 pub fn print_adm_xml_summary(adm_xml: []const u8, writer: AnyWriter) !void {
     xml.xmlInitParser();
     defer xml.xmlCleanupParser();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
     const doc: xml.xmlDocPtr = xml.xmlReadMemory(@ptrCast(adm_xml), @intCast(adm_xml.len), null, "utf-8", 0) orelse {
         return error.XmlParseError;
@@ -48,7 +107,10 @@ pub fn print_adm_xml_summary(adm_xml: []const u8, writer: AnyWriter) !void {
         @panic("xmlXPathRegisterNs Failed!");
     }
 
-    try print_audio_programme_data(doc, xpath_ctx, writer);
+    const programme = AdmAudioProgramme.init(allocator, xpath_ctx);
+    defer programme.deinit();
+
+    try programme.print(writer);
 }
 
 test "parse XML from data buffer" {
@@ -64,10 +126,3 @@ test "parse XML from data buffer" {
         try std.testing.expect(std.mem.eql(u8, std.mem.span(r.*.name), "x"));
     }
 }
-
-// test "XML error from data buffer" {
-//     const buffer: [*:0]const u8 = "<xa</x>";
-//     const doc: ?xml.xmlDocPtr = xml.xmlReadMemory(buffer, 8, null, "utf-8", 0);
-//     defer xml.xmlCleanupParser();
-//     defer xml.xmlFreeDoc(doc);
-// }
---
+++ src/xml_additions.zig
@@ -0,0 +1,113 @@
+const std = @import("std");
+const xml = @cImport({
+    @cInclude("libxml2/libxml/parser.h");
+    @cInclude("libxml2/libxml/tree.h");
+    @cInclude("libxml2/libxml/xpath.h");
+    @cInclude("libxml2/libxml/xpathInternals.h");
+    @cInclude("libxml2/libxml/xmlmemory.h");
+});
+const Allocator = std.mem.Allocator;
+// const ADMError = error{ XmlParseError, AdmXmlEntityError };
+
+pub const XPathNodeIter = struct {
+    result: xml.xmlXPathObjectPtr,
+    i: usize,
+
+    fn init(result: xml.xmlXPathObjectPtr) @This() {
+        if (result.*.type != xml.XPATH_NODESET) {
+            @panic("XPathNodeIter init expression did not evaluate to a node set!");
+        }
+
+        return @This(){
+            .result = result,
+            .i = 0,
+        };
+    }
+
+    fn empty(self: @This()) bool {
+        return (self.*.nodesetval.*.nodeNr == 0);
+    }
+
+    fn next(self: *@This()) ?xml.xmlNodePtr {
+        if (self.i >= self.result.*.nodesetval.*.nodeNr) {
+            return null;
+        } else {
+            defer self.i += 1;
+            return self.result.*.nodesetval.*.nodeTab[self.i];
+        }
+    }
+
+    fn deinit(self: @This()) void {
+        xml.xmlXPathFreeObject(self.result);
+    }
+};
+
+/// Evaluates query against the given xpath_ctx and returns an XpathObjectPtr.
+/// If node_ctx is given, this node is set as the context node for the request,
+/// and then the original context node is restored at the end of the function.
+/// Panics if xmlXPathEvalExpression retunrs a null.
+/// The returned value must be freed by the caller with xmlXPathFreeObject()
+fn xpath_eval_with_context_node(expression: []const u8, xpath_ctx: xml.xmlXPathContextPtr, node_ctx: ?xml.xmlNodePtr) xml.xmlXPathObjectPtr {
+    var orig_node: ?xml.xmlNodePtr = null;
+    if (node_ctx) |node| {
+        orig_node = xpath_ctx.*.node;
+        if (xml.xmlXPathSetContextNode(node, xpath_ctx) != 0) {
+            @panic("Failed to set context node!");
+        }
+    }
+
+    if (xml.xmlXPathEvalExpression(@ptrCast(expression), xpath_ctx)) |result| {
+        return result;
+    } else {
+        @panic("xmlXPathEvalExpression returned null!");
+    }
+
+    if (orig_node) |node| {
+        if (xml.xmlXPathSetContextNode(node, xpath_ctx) != 0) {
+            @panic("Failed to restore original contex node!");
+        }
+    }
+}
+
+/// Runs an XPath query against the xpath_ctx and returns a []u8.
+/// The return value must be freed by the caller.
+/// If the query does not result in a string return value, this function will
+/// panic.
+pub fn xpath_string_value(xpath: []const u8, xpath_ctx: xml.xmlXPathContextPtr, allocator: Allocator) []const u8 {
+    const result = xpath_eval_with_context_node(xpath, xpath_ctx, null);
+    defer xml.xmlXPathFreeObject(result);
+
+    switch (result.*.type) {
+        xml.XPATH_STRING => {
+            const str_spanned = std.mem.span(result.*.stringval);
+            const retval = allocator.alloc(u8, str_spanned.len) catch {
+                @panic("Failed to allocate memory for XPath string value!");
+            };
+            std.mem.copyForwards(u8, retval, str_spanned);
+
+            return retval;
+        },
+        else => {
+            @panic("XPath parameter to xpath_string_value() does not have string result!");
+        },
+    }
+}
+
+/// Runs an XPath query against the xpath_ctx and returns a []u8.
+/// The return value must be freed by the caller.
+/// If the query does not result in a string return value, this function will
+/// panic.
+pub fn xpath_nodeset_value(xpath: []const u8, xpath_ctx: xml.xmlXPathContextPtr) XPathNodeIter {
+    const result = xml.xmlXPathEvalExpression(@ptrCast(xpath), xpath_ctx) orelse {
+        @panic("xmlXPathEvalExpression returned null!");
+    };
+
+    switch (result.*.type) {
+        xml.XPATH_NODESET => {
+            return XPathNodeIter.init(result);
+        },
+        else => {
+            @panic("XPath parameter to xpath_nodeset_value() does not have nodeset result!");
+        },
+    }
+}
